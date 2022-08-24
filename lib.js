9a484b89d17d966b99847814b159443ca5421926
ixguza <rtk0253@gmail.com>
2022-08-24 23:47:27 +0900

    025

---
+++ lib.js
@@ -0,0 +1,90 @@
+function log(...data) {
+    console.log(...data);
+    const db = document.getElementById('debugbox');
+    if (db) {
+        db.innerText += `${data.join(" ")}\n`
+    }
+}
+
+function toColorCode(r, g, b) {
+    return "#" + [r, g, b].map((x) => parseInt((x + 256)).toString(16).slice(1)).join("");
+}
+
+// https://lab.syncer.jp/Web/JavaScript/Snippet/67/
+function hsv2rgb(h, s, v) {
+    var h = h / 60;
+    if (s == 0) return [v * 255, v * 255, v * 255];
+
+    var rgb;
+    var i = parseInt(h);
+    var f = h - i;
+    var v1 = v * (1 - s);
+    var v2 = v * (1 - s * f);
+    var v3 = v * (1 - s * (1 - f));
+
+    switch (i) {
+        case 0:
+        case 6:
+            rgb = [v, v3, v1];
+            break;
+
+        case 1:
+            rgb = [v2, v, v1];
+            break;
+
+        case 2:
+            rgb = [v1, v, v3];
+            break;
+
+        case 3:
+            rgb = [v1, v2, v];
+            break;
+
+        case 4:
+            rgb = [v3, v1, v];
+            break;
+
+        case 5:
+            rgb = [v, v1, v2];
+            break;
+    }
+
+    return rgb.map(function (value) {
+        return value * 255;
+    });
+}
+
+function rgb2hsv(r, g, b) {
+    var r = r / 255;
+    var g = g / 255;
+    var b = b / 255;
+
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var diff = max - min;
+
+    var h = 0;
+
+    switch (min) {
+        case max:
+            h = 0;
+            break;
+
+        case r:
+            h = (60 * ((b - g) / diff)) + 180;
+            break;
+
+        case g:
+            h = (60 * ((r - b) / diff)) + 300;
+            break;
+
+        case b:
+            h = (60 * ((g - r) / diff)) + 60;
+            break;
+    }
+
+    var s = max == 0 ? 0 : diff / max;
+    var v = max;
+
+    return [h, s, v];
+}
--- main.js
+++ main.js
@@ -1,39 +1,135 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const [width, height] = [200, 200];
-canvas.width = width;
-canvas.height = height;
+function checkReload() {
+    const searchmap = Object.fromEntries(document.location.search.slice(1).split("&").map(a => a.split("=")));
 
-const img = document.getElementById('img');
-img.addEventListener('load', function () {
-    console.log("load")
-    ctx.drawImage(img, 0, 0, width, height);
-
-    for (const ev of ['mousemove'/*, 'touchmove'*/]) {
-        console.log("ev")
-        canvas.addEventListener(ev, (e) => { handler(e) })
+    const t = searchmap._ || 0
+    const now = Date.now();
+    if (now - t > 10 * 1000) {
+        document.location.search = `?_=${now}`
     }
-    // a();
-}, false);
-
-function handler(e) {
-    const [x, y] = [e.offsetX, e.offsetY];
-    const imgData = ctx.getImageData(x, y, 1, 1);
-    const [r, g, b, a] = imgData.data;
-    // console.log({x,y,r,g,b,a})
-    const cs = document.querySelector('.color-square');
-    const ct = document.querySelector('.color-text');
-    const clrcd = toColorCode(r, g, b);
-    cs.style = `background-color:${clrcd}`;
-    ct.innerText = clrcd;
-    //    drawLine(context, x, y, e.offsetX, e.offsetY);
-}
-function a() {
-    const a = 2
-    document.body.innerHTML = `<div>${a}</div>` + document.body.innerHTML
-}
-function toColorCode(r, g, b) {
-    return "#" + [r, g, b].map((x) => (x + 256).toString(16).slice(1)).join("");
 }
 
-console.log("ok")
+function main() {
+    checkReload();
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const [width, height] = [200, 200];
+    canvas.width = width;
+    canvas.height = height;
+
+    const inputImg = document.getElementById('input-img')
+    inputImg.addEventListener('change', (e) => {
+        const image = new Image();
+
+        const file = e.target.files[0]
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            image.src = reader.result;
+        }
+
+        image.onload = () => {
+            log("load")
+            ctx.drawImage(image, 0, 0, width, height);
+
+            for (const ev of ['mousemove'/*, 'touchmove'*/]) {
+                log("ev")
+                canvas.addEventListener(ev, (e) => { handler(e) })
+            }
+            // a();
+        }
+    });
+
+    function handler(e) {
+        const [x, y] = [e.offsetX, e.offsetY];
+        const imgData = ctx.getImageData(x, y, 1, 1);
+        const [r, g, b, a] = imgData.data;
+        // console.log({x,y,r,g,b,a})
+        const cs = document.querySelector('.color-square');
+        const ct = document.querySelector('.color-text');
+        const clrcd = toColorCode(r, g, b);
+        cs.style = `background-color:${clrcd}`;
+        ct.innerText = clrcd;
+        createColorCircle(100, r, g, b)
+        //    drawLine(context, x, y, e.offsetX, e.offsetY);
+    }
+}
+
+function createColorCircle(size, r, g, b) {
+    const clrc = document.getElementById('color-circle');
+    clrc.width = size; clrc.height = size;
+    const ctx = clrc.getContext('2d');
+
+    const r1 = size / 2
+    const r2 = r1 * 3 / 4
+    const sr = r2 / 1.41
+
+    const x0 = size / 2
+    const y0 = size / 2
+
+    const N = 36
+    for (let i = 0; i < N; i++) {
+        const region = new Path2D();
+
+        const [_r, _g, _b] = hsv2rgb(360 * i / N, 1, 1);
+        // console.log({ i }, { r, g, b }, [360 * i / N, 1, 1])
+
+        ctx.fillStyle = toColorCode(_r, _g, _b);
+
+        let moved = false;
+
+        const dr12 = [r1, r1, r2, r2];
+        const di = [i - 0.5, i + 0.5, i + 0.5, i - 0.5];
+
+        [0, 1, 2, 3].map(ii => {
+            const theta = Math.PI * 5 / 6 - Math.PI * 2 / N * di[ii];
+            return [dr12[ii] * Math.cos(theta), dr12[ii] * Math.sin(theta)]
+        }).forEach(([_x, _y]) => {
+            const xx = x0 + _x;
+            const yy = y0 - _y;
+            if (!moved) {
+                region.moveTo(xx, yy);
+                moved = true;
+            } else {
+                region.lineTo(xx, yy);
+            }
+        });
+        region.closePath();
+        ctx.fill(region);
+    }
+
+    const [h, s, v] = rgb2hsv(r, g, b);
+    const M = 20;
+    for (let i = 0; i < M; i++) {
+        for (let jj = 0; jj < M; jj++) {
+            const xx = x0 - sr + (sr * 2) * i / M;
+            const yy = y0 - sr + (sr * 2) * (M - 1 - jj) / M;
+            const [rr, gg, bb] = hsv2rgb(h, i / M, jj / M)
+            ctx.fillStyle = toColorCode(rr, gg, bb);
+            ctx.fillRect(xx, yy, sr * 2 / M, sr * 2 / M);
+        }
+    }
+
+    const markerSize = 4;
+    // hue
+    function markHUE() {
+        const angle = Math.PI * 5 / 6 - Math.PI * 2 * h / 360;
+        const rr = (r1 + r2) / 2;
+        const xx = x0 + rr * Math.cos(angle);
+        const yy = y0 - rr * Math.sin(angle);
+        ctx.fillStyle = "white";
+        ctx.fillRect(xx - markerSize / 2, yy - markerSize / 2, markerSize, markerSize);
+    }
+    markHUE();
+    // s,v
+    function markSV() {
+        const xx = x0 - sr + (2 * sr * s);
+        const yy = y0 - sr + (2 * sr * (1 - v));
+        ctx.fillStyle = "white";
+        ctx.fillRect(xx - markerSize / 2, yy - markerSize / 2, markerSize, markerSize);
+    }
+    markSV();
+    log({ size, r1, r2, sr, x0, y0 });
+    log({ r, g, b, h, s, v });
+}
+
+document.addEventListener('DOMContentLoaded', main);
